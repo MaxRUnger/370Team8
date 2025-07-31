b83e33ca6f6a3527ab33fe7870e9d0104ced3d56
tyson014 <tyson014@csusm.edu>
2025-07-30 21:22:26 -0700

    Add files via upload
    Added the recommendations & previous mood feature.

--- moodtracker.h
+++ moodtracker.h
@@ -2,12 +2,19 @@
 #define MOODTRACKER_H
 
 #include <QWidget>
+#include <QString> // This is needed for QString.
+
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
 class MoodTracker;
 }
 QT_END_NAMESPACE
+
+struct Recommendation {
+    QString shortTask;
+    QString longTask;
+};
 
 class MoodTracker : public QWidget
 {
@@ -17,10 +24,30 @@ public:
     MoodTracker(QWidget *parent = nullptr);
     ~MoodTracker();
 
+signals:
+    void goToLoginPage();
+    void goToJournalPage();
+    void goToTasksPage();
+
 private slots:
     void handleSubmit();
+    void handleLoginClicked();
+    void handleJournalClicked();
+    void handleTasksClicked();
+
 
 private:
     Ui::MoodTracker *ui;
+    // Adding my function declarations for the recommendation logic
+
+    int previousMood = 0;
+    int previousEnergy = 0;
+    int previousAnxiety = 0;
+
+
+    Recommendation getMoodRecommendation(int mood);
+    Recommendation getAnxietyRecommendation(int anxiety);
+    Recommendation getEnergyRecommendation(int energy);
+
 };
 #endif // MOODTRACKER_H
